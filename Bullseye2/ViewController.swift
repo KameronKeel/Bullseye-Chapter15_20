690de0f696cd1754e31d7d996b63481902a000a8
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-09-04 16:36:13 -0400

    Completed Chapter 16

--- Bullseye2/ViewController.swift
+++ Bullseye2/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 50
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showAlert(){
+        let message = "The value of the slider is: \(currentValue)"
         let alert = UIAlertController(title: "Hello World",message:"This is my first app!", preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
     }
 }
 
