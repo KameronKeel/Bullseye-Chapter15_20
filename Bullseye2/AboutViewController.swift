10194e56916707cc8efa3f26bc1a8eff809d4b04
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-09-06 16:13:39 -0400

    Completed Chapter 19

---
+++ Bullseye2/AboutViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.swift
+//  Bullseye2
+//
+//  Created by Kameron Keel on 9/6/20.
+//  Copyright © 2020 Kameron Keel. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = Bundle.main.url(forResource: "Bullseye", withExtension: "html"){
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+        @IBAction func close(){
+            dismiss(animated:true, completion: nil)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
